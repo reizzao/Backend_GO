63d134774b1929cfc03826f7add472754db96b71
reizzao <reizao1977@gmail.com>
2025-05-28 12:05:51 -0300

    feat: [cliente1] add implementacao do salvamento de dado em repositorio memory

--- entities/example1/repositories/memory.repository.go
+++ entities/example1/repositories/memory.repository.go
@@ -4,8 +4,13 @@ import types "github.com/reizzao/backendgo/entities/example1/types"
 
 var MemoryRepositoryExample1 = types.RepositoryExample1{
 
+	Collection: []types.Example1Model{},
+
 	Save: func(model types.Example1Model) types.Example1Model {
-		// todo: implementar - salvar na base de dados - Json
+
+		this := types.RepositoryExample1{} // recebendo o obj pra usar props
+		_ = append(this.Collection, model) // nao usando o retorno ??? todo
+
 		return model
 	},
 }
